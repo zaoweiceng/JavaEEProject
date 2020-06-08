99ed2a5d16645200a1e330d0fc146ea05ab4d9c5
zaoweiceng <zaoweiceng@github,com>
2020-06-08 22:57:27 +0800

    utile1

---
+++ src/edu/cuit/lm/util/DBUtile.java
@@ -0,0 +1,34 @@
+package edu.cuit.lm.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class  DBUtile {
+    private static InputStream in;
+    private static SqlSessionFactoryBuilder builder;
+    private static SqlSessionFactory factory;
+    private static SqlSession sqlSession;
+
+    public static void init() throws IOException {
+        in =  Resources.getResourceAsStream("mybatisConf.xml");
+        builder =  new SqlSessionFactoryBuilder();
+        factory  = builder.build(in);
+        sqlSession = factory.openSession();
+    }
+
+    public static void commit(){
+        sqlSession.commit();
+    }
+
+    public static SqlSession getSqlSession(){return sqlSession;}
+
+    public static void close() throws IOException {
+        in.close();
+        sqlSession.close();
+    }
+}
---
+++ src/edu/cuit/lm/util/JDBCUtile.java
@@ -0,0 +1,27 @@
+package edu.cuit.lm.util;
+
+import edu.cuit.lm.dao.accountDao;
+import edu.cuit.lm.entity.account;
+import edu.cuit.lm.entity.userInf;
+
+import java.io.IOException;
+
+public class JDBCUtile {
+    public boolean addAcount(account a) throws IOException {
+        if (a != null){
+            daoImp.getAccount().insertInto(a);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addUser(userInf user){
+        if (user != null){
+            daoImp.getUserInf().insertInto(user);
+            return true;
+        }
+        return false;
+    }
+
+
+}
