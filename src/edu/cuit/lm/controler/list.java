999c4d199f71458ec8f141a944e807f8acb9984d
zaoweiceng <zaoweiceng@github,com>
2020-06-10 16:11:48 +0800

    pri12

---
+++ src/edu/cuit/lm/controler/list.java
@@ -0,0 +1,30 @@
+package edu.cuit.lm.controler;
+
+import edu.cuit.lm.entity.userInf;
+import edu.cuit.lm.util.JDBCUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+@WebServlet("/findAll")
+public class list extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        JDBCUtil jd = new JDBCUtil();
+        List<userInf> list_Login = jd.findUserAll();
+        req.setAttribute("list_Login", list_Login);
+        req.getRequestDispatcher("/admin.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
