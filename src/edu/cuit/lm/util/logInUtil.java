9cae139d2bccc572cbb9f3ea7ff74e4347f54bb0
zaoweiceng <zaoweiceng@github,com>
2020-06-10 12:02:26 +0800

    pri

--- src/edu/cuit/lm/util/logInUtil.java
+++ src/edu/cuit/lm/util/logInUtil.java
@@ -3,15 +3,16 @@ package edu.cuit.lm.util;
 import edu.cuit.lm.entity.userInf;
 
 public class logInUtil {
-    public static boolean check(int id, String pwd){
+    public static int check(int id, String pwd){
         JDBCUtil jd = new JDBCUtil();
         userInf userInfById = jd.findUserInfById(id);
         if (userInfById == null){
-            return false;
+            return -1;
         }
         if (userInfById.getpWd().equals(pwd)){
-            return true;
+            Integer priLv = daoImp.getUserPri().findUserById(userInfById.getIdUser()).getPriLv();
+            return priLv;
         }
-        return false;
+        return -1;
     }
 }
