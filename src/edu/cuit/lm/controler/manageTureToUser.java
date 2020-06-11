3d88b8e6b6a44461908d428765e79f9018f3f1fa
TMDK-LYY <l2385400407@gmail.com>
2020-06-11 11:34:21 +0800

    Default Changelist

--- src/edu/cuit/lm/controler/manageTureToUser.java
+++ src/edu/cuit/lm/controler/manageTureToUser.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 /*
  * Servlet implementation class DeleteUser
  */
-@WebServlet({ "/manageTureToUser"})
+@WebServlet({ "/manageTurnToUser"})
 public class manageTureToUser extends HttpServlet {
     private static final long serialVersionUID =1L;
 
